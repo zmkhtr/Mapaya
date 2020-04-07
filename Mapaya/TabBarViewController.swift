20edde64ce14d37af801c1554aadbd5ca2dd4681
zmkhtr <azammukhtar3@gmail.com>
2020-04-07 11:45:23 +0700

    "set storyboard"

---
+++ Mapaya/TabBarViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarViewController.swift
+//  Mapaya
+//
+//  Created by Azam Mukhtar on 07/04/20.
+//  Copyright © 2020 Azam Mukhtar. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
