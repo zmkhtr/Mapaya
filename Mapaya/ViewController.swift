23d43a4e60df4d3ff77b1a28d59004449fd8f0cf
zmkhtr <azammukhtar3@gmail.com>
2020-04-07 09:09:47 +0700

    Initial Commit

---
+++ Mapaya/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mapaya
+//
+//  Created by Azam Mukhtar on 07/04/20.
+//  Copyright © 2020 Azam Mukhtar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
