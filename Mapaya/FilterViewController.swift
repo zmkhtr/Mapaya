775b91112145392498ca59fff9e54c66a0b4d978
Stendy Antonio <stendy20.sa@gmail.com>
2020-04-08 21:15:57 +0700

    Gw buat FIlter view controller di main storyboard

---
+++ Mapaya/FilterViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FilterViewController.swift
+//  Mapaya
+//
+//  Created by Stendy Antonio on 08/04/20.
+//  Copyright © 2020 Azam Mukhtar. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func MeatButton(_ sender: Any) {
+    }
+    
+    @IBAction func DismissButton(_ sender: Any) {
+    }
+    
+    @IBAction func DoneButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
