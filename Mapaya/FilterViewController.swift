d944bfd66796455e667f4c935c2f2c22f5e9093c
Stendy Antonio <stendy20.sa@gmail.com>
2020-04-09 11:51:43 +0700

    penambahan di bagian filter

--- Mapaya/FilterViewController.swift
+++ Mapaya/FilterViewController.swift
@@ -9,14 +9,58 @@
 import UIKit
 
 class FilterViewController: UIViewController {
-
+    
+    var clicked = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        DropDown.isHidden = true
         // Do any additional setup after loading the view.
     }
     
+    //DAIRY
+    @IBOutlet weak var DairyButtonOutlet: UIButton!
+    @IBOutlet weak var DairyLogoOutlet: UIImageView!
+    @IBOutlet weak var DairyLabelOutlet: UILabel!
+    
+    //MEAT
+    @IBOutlet weak var MeatButtonOutlet: UIButton!
+    
+    //VEGETABLE
+    @IBOutlet weak var VegetableButtonOutlet: UIButton!
+    
+    //DROPDOWN
+    @IBOutlet weak var DropDown: UIView!
+    
     @IBAction func MeatButton(_ sender: Any) {
+        if clicked == 0 {
+            DropDown.isHidden = false
+            clicked = 1
+            MeatButtonOutlet.frame = CGRect(x: 20, y: 455, width: 102, height: 130)
+            DairyButtonOutlet.frame = CGRect(x: 20, y: 730, width: 102, height: 115)
+        }
+        else{
+            DropDown.isHidden = true
+            clicked = 0
+            MeatButtonOutlet.frame = CGRect(x: 20, y: 455, width: 102, height: 115)
+            DairyButtonOutlet.frame = CGRect(x: 20, y: 595, width: 102, height: 115)
+            
+        }
+    }
+    
+    @IBAction func VegetableButton(_ sender: Any) {
+        if clicked == 0 {
+            DropDown.isHidden = false
+            clicked = 1
+            VegetableButtonOutlet.frame = CGRect(x: 155, y: 455, width: 102, height: 130)
+            DairyButtonOutlet.frame = CGRect(x: 20, y: 730, width: 102, height: 115)
+        }
+        else{
+            DropDown.isHidden = true
+            clicked = 0
+            VegetableButtonOutlet.frame = CGRect(x: 155, y: 455, width: 102, height: 115)
+            DairyButtonOutlet.frame = CGRect(x: 20, y: 595, width: 102, height: 115)
+        }
     }
     
     @IBAction func DismissButton(_ sender: Any) {
@@ -25,15 +69,15 @@ class FilterViewController: UIViewController {
     @IBAction func DoneButton(_ sender: Any) {
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
